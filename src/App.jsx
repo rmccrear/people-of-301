82562928239d30b0fbdbf88e124f71fcb9e702d2
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-07-07 10:22:23 +0800

    add comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 // step 1 import React
 import React from 'react';
+
+// All of our components will be put in a 
+// folder called components.
 import Person from './components/Person';
 import Header from './components/Header'
 
@@ -7,6 +10,7 @@ import Header from './components/Header'
 // step2 create a component
 class App extends React.Component {
 
+  // we have a render function that returns JSX.
   render() {
     return (
       <div>
